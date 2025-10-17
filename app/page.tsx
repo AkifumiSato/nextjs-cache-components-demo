6ded961069a417321a4661e24141d4f7805e6007
佐藤昭文 <akfm.sato@gmail.com>
2025-10-17 16:28:32 +0900

    cache fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+"use cache"
+import { cacheLife } from 'next/cache'
+
+export default async function Page() {
+  cacheLife("seconds")
+
+  const todo = await fetch('https://dummyjson.com/todos/random')
+    .then(res => res.json())
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <pre>
+        <code>
+          {JSON.stringify(todo)}
+        </code>
+      </pre>
+    </div>
+  );
 }
