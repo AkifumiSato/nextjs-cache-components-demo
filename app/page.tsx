6c56459f872633896f58c271c3b6039072897c06
佐藤昭文 <akfm.sato@gmail.com>
2026-01-31 20:28:46 +0900

    formatter, AGENTS.md

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-"use cache"
-import { cacheLife } from 'next/cache'
+"use cache";
+
+import { cacheLife } from "next/cache";
 
 export default async function Page() {
-  cacheLife("seconds")
+  cacheLife("seconds");
 
-  const todo = await fetch('https://dummyjson.com/todos/random')
-    .then(res => res.json())
+  const todo = await fetch("https://dummyjson.com/todos/random").then((res) =>
+    res.json(),
+  );
+
   return (
     <div>
       <h1>Hello, Next.js!</h1>
       <pre>
-        <code>
-          {JSON.stringify(todo)}
-        </code>
+        <code>{JSON.stringify(todo)}</code>
       </pre>
     </div>
   );
